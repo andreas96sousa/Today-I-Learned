47fc41b5cca9410c452095c892ab5142956a7733
unknown <carlossousa0691@digitalcollege.internal>
2026-05-07 21:54:56 -0300

    feat: introdução as funcionalidades de props, estado, renderizaçãp condicional e renderização de litas

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,129 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+// function Button(props){
 
-function App() {
-  const [count, setCount] = useState(0)
+//   function clicar(){
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+//   }
 
-      <div className="ticks"></div>
+//     return <button>{props.text}</button>
+// }
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+// export default function App() {
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+//   return (
+//     <>
+//     <h1 id='principal'>
+//       Hello World
+//     </h1>
+//     <Button text='Button 01' script='button1'/>
+//     <Button text='Outro Button' script='button2'/>
+//     </>
+//   )
+// }
+
+
+// export default function App(){
+
+//  const [counter, setCounter] = useState(0)
+
+
+
+//  function count(){
+//   setCounter(counter + 1)
+//   console.log(counter)
+
+//  }
+
+
+
+
+//   return(
+  //     <>  
+//       <span>{counter}</span> <hr />
+//       <button onClick={count}>Click</button>
+//     </>
+//   )
+// }
+
+// export default function App(){
+// const aluno = {
+//   id: 1,
+//   nome: 'Andreas Sousa',
+//   curso: 'Fullstack',
+//   status: false
+// }
+
+// return (
+//   // <>
+//   //   {aluno.status && (
+  //   //   `O aluno ${aluno.nome} está ${aluno.status}`)
+//   //   }
+//   // </>
+
+//   // <>
+//   //   {aluno.status ? 'Aluno Ativo' : 'Aluno Passivo'}  
+//   // </>
+
+//   <>
+
+//   </>
+
+// )
+// }
+
+function NewFact(props){
+  return(
+    <>  
+      <p>{props.text}</p>
+      <a href={props.source} target="_blank">Fonte</a>
     </>
+
   )
 }
 
-export default App
+
+import { useState } from "react"
+import type { Fact } from "./types"
+
+
+export default function App(){
+  const facts: Fact[] = [
+    {
+      id: 1,
+      text: 'FATO 01',
+      source: 'https://google.com/q=fato1',
+      category: 'Tecnologia',
+      votes_interesting: 1,
+      votes_mindblowing: 2,
+      votes_false: 3,
+      create_at: '2026-05-07 19:30:05 GMT-3',
+    },
+    {
+      id: 2,
+      text: 'FATO 02',
+      source: 'https://google.com/q=fato2',
+      category: 'Tecnologia',
+      votes_interesting: 3,
+      votes_mindblowing: 2,
+      votes_false: 1,
+      create_at: '2026-05-06 13:32:35 GMT-3',
+    }
+  ]
+
+
+  return(
+    <>  
+      {
+        facts.map((fact) => {
+          return(
+            <NewFact 
+              key= {fact.id}
+              text= {fact.text}
+              source= {fact.source}
+            />
+          )
+          }
+        )
+      }  
+    </>
+  )
+}
