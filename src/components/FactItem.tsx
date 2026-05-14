cab22c06faa342de409d5f78d0688edf1b8e2da2
unknown <carlossousa0691@digitalcollege.internal>
2026-05-14 19:45:24 -0300

    feat: implementar componente de item para lista de fatos

---
+++ src/components/FactItem.tsx
@@ -0,0 +1,24 @@
+import type { Fact } from "../types"
+
+interface FactItemProps{
+    fact: Fact
+}
+
+
+export default function FactItem({fact}: FactItemProps){
+
+
+
+
+
+    return(
+        <>
+           <li>
+            <p>{fact.text}</p>
+            <a href={fact.source}>(Source)</a>
+            <span>{fact.category}</span>
+
+           </li>
+        </>
+    )
+}
