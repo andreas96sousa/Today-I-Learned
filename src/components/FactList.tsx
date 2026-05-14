02a55c7da2b131e2bcb6839af152206c2238ff3d
unknown <carlossousa0691@digitalcollege.internal>
2026-05-14 20:02:08 -0300

    feat: implementat componente de lista de fatos

---
+++ src/components/FactList.tsx
@@ -0,0 +1,24 @@
+import FactItem from "./FactItem";
+import type { Fact } from "../types";
+
+interface FactListProps{
+    facts: Fact[]
+}
+
+export default function FactList({facts}: FactListProps){
+    if(facts.length == 0){
+        <p>Nenhum fato para esta categoria ainda, crie o primeiro! ✌️</p> 
+    }
+
+
+
+    return(
+        <>
+            {
+                facts.map(fact => {
+                    return <FactItem key={fact.id} fact={fact} />
+                })
+            }
+        </>
+    )
+}
