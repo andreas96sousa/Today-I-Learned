849c7245867ec1771dc6d24c207a76376a4f4c29
unknown <carlossousa0691@digitalcollege.internal>
2026-05-14 19:17:36 -0300

    chore: definir contrato constante de categorias

---
+++ src/constants.ts
@@ -0,0 +1,40 @@
+import type { Category } from "./types"
+
+export const CATEGORIES: Category[] = [
+    {
+        value: 'tecnology',
+        label: 'tecnologia',    
+        color: 'bg-blue-500'
+    },{
+        value: 'science',
+        label: 'ciência',    
+        color: 'bg-green-500'
+    },{
+        value: 'finance',
+        label: 'finanças',    
+        color: 'bg-red-500'
+    },{
+        value: 'society',
+        label: 'sociedade',    
+        color: 'bg-yellow-500'
+    },{
+        value: 'enterteinment',
+        label: 'entretenimento',    
+        color: 'bg-pink-500'
+    },{
+        value: 'health',
+        label: 'saúde',    
+        color: 'bg-teal-500'
+    },{
+        value: 'history',
+        label: 'história',    
+        color: 'bg-orange-500'
+    },{
+        value: 'news',
+        label: 'notícia',    
+        color: 'bg-violet-500'
+    }
+    
+]
+
+
